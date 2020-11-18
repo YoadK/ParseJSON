07f4d914058a48a98581d7ac939616e0c66392a2
YoadK <yoadkot@gmail.com>
2020-11-18 15:39:41 -0800

    update

---
+++ app/src/main/java/com/example/parsejson/OtherPackage/EmailPasswordPhoneValidator.java
@@ -0,0 +1,62 @@
+package com.example.parsejson.OtherPackage;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//used by LoginActivity / RegisterActivity/ UserProfileFragment
+//in order to validate Email/ Password/ Phone number
+public class EmailPasswordPhoneValidator {
+
+    private static volatile EmailPasswordPhoneValidator sInstance;
+
+    private EmailPasswordPhoneValidator() {
+        if (sInstance != null) {
+            throw new RuntimeException("Use getInstance() method to get the single instance of this class.");
+        }
+    }
+
+    public static EmailPasswordPhoneValidator getInstance() {
+        if (sInstance == null) {
+            synchronized (EmailPasswordPhoneValidator.class) {
+                if (sInstance == null) sInstance = new EmailPasswordPhoneValidator();
+            }
+        }
+
+        return sInstance;
+    }
+
+    private static String EMAIL_PATTERN =
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                    "\\@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+";
+    private static String PHONE_NUMBER_PATTERN = "[0-9]{10}$";
+    private static Pattern pattern;
+    private static Matcher matcher;
+
+    public boolean isValidEmail(final String email) {
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+
+        return matcher.matches();
+    }
+
+    public boolean isValidPassword(final String password) {
+        if (password.length() < 6) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean isValidPhoneNumber(final String phone_number) {
+        pattern = Pattern.compile(PHONE_NUMBER_PATTERN);
+        matcher = pattern.matcher(phone_number);
+
+        return matcher.matches();
+    }
+
+}
---
+++ app/src/test/java/com/example/parsejson/EmailPasswordPhoneValidatorTest.java
@@ -0,0 +1,114 @@
+package com.example.parsejson;
+
+import com.example.parsejson.OtherPackage.EmailPasswordPhoneValidator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit tests for the EmailPasswordPhoneValidator logic.
+ */
+public class EmailPasswordPhoneValidatorTest {
+
+    /**
+     * Test Emails.
+     */
+    @Test
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertTrue(EmailPasswordPhoneValidator.getInstance().isValidEmail("name@email.com"));
+    }
+
+    @Test
+    public void emailValidator_CorrectEmailSubDomain_ReturnsTrue() {
+        assertTrue(EmailPasswordPhoneValidator.getInstance().isValidEmail("name@email.co.uk"));
+    }
+
+    @Test
+    public void emailValidator_InvalidEmailNoTld_ReturnsFalse() {
+        assertFalse(EmailPasswordPhoneValidator.getInstance().isValidEmail("name@email"));
+    }
+
+    @Test
+    public void emailValidator_InvalidEmailDoubleDot_ReturnsFalse() {
+        assertFalse(EmailPasswordPhoneValidator.getInstance().isValidEmail("name@email..com"));
+    }
+
+    @Test
+    public void emailValidator_InvalidEmailNoUsername_ReturnsFalse() {
+        assertFalse(EmailPasswordPhoneValidator.getInstance().isValidEmail("@email.com"));
+    }
+
+    @Test
+    public void emailValidator_EmptyString_ReturnsFalse() {
+        assertFalse(EmailPasswordPhoneValidator.getInstance().isValidEmail(""));
+    }
+
+    /**
+     * Test Passwords.
+     */
+    @Test
+    public void passwordValidator_CorrectPasswordSimpleNumbers_ReturnsTrue() {
+        assertTrue(EmailPasswordPhoneValidator.getInstance().isValidPassword("123456"));
+    }
+
+    @Test
+    public void passwordValidator_CorrectPasswordSimpleSmallWords_ReturnsTrue() {
+        assertTrue(EmailPasswordPhoneValidator.getInstance().isValidPassword("qwerty"));
+    }
+
+    @Test
+    public void passwordValidator_CorrectPasswordSimpleBigWords_ReturnsTrue() {
+        assertTrue(EmailPasswordPhoneValidator.getInstance().isValidPassword("QWERTY"));
+    }
+
+    @Test
+    public void passwordValidator_CorrectPasswordMulti_ReturnsTrue() {
+        assertTrue(EmailPasswordPhoneValidator.getInstance().isValidPassword("12qwER"));
+    }
+
+    @Test
+    public void passwordValidator_InvalidEmptyPassword_ReturnsFalse() {
+        assertFalse(EmailPasswordPhoneValidator.getInstance().isValidPassword(""));
+    }
+
+    @Test
+    public void passwordValidator_InvalidPasswordTooMuch_ReturnsTrue() {
+        assertTrue(EmailPasswordPhoneValidator.getInstance().isValidPassword("1234567qwertyuQWERTYU"));
+    }
+
+    @Test
+    public void passwordValidator_InvalidPasswordLengthSevenChars_ReturnsTrue() {
+        assertTrue(EmailPasswordPhoneValidator.getInstance().isValidPassword("Abcdefg"));
+    }
+
+    /**
+     * Test PhoneNumbers.
+     */
+    @Test
+    public void phoneNumbersValidator_CorrectPhoneNumbersSimpleNumbers_ReturnsTrue() {
+        assertTrue(EmailPasswordPhoneValidator.getInstance().isValidPhoneNumber("0501234567"));
+    }
+
+    @Test
+    public void phoneNumbersValidator_InvalidEmptyPhoneNumbers_ReturnsFalse() {
+        assertFalse(EmailPasswordPhoneValidator.getInstance().isValidPhoneNumber(""));
+    }
+
+    @Test
+    public void phoneNumbersValidator_InvalidPhoneNumbersTooMuch_ReturnsFalse() {
+        assertFalse(EmailPasswordPhoneValidator.getInstance().isValidPhoneNumber("05012345678"));
+    }
+
+    @Test
+    public void phoneNumbersValidator_InvalidPhoneNumbersLittleToo_ReturnsFalse() {
+        assertFalse(EmailPasswordPhoneValidator.getInstance().isValidPhoneNumber("050123456"));
+    }
+
+    @Test
+    public void phoneNumbersValidator_InvalidPhoneNumbersPhoneNumbersShort_ReturnsFalse() {
+        assertFalse(EmailPasswordPhoneValidator.getInstance().isValidPhoneNumber("054544690"));
+    }
+
+}
